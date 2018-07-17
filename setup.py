ba961bb5f4734172ca66864ea5b8148e29fc4ac2
Raymond Butcher <ray.butcher@claranet.uk>
2018-07-17 14:20:05 +0000

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='libfaketimefs-botocore',
-    version='0.0.1',
+    version='0.0.2',
     description='Patches botocore to work with libfaketimefs',
     author='Raymond Butcher',
     author_email='ray.butcher@claranet.uk',
